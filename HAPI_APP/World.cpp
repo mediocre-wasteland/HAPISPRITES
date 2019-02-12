b9f65eba2c70daf155f707ab233ca0f5c9c4bdb2
Jsimo42 <jsimo42@gmail.com>
2019-02-12 12:27:57 +0000

    V1.1 - Added World Class and moved everything from main.cpp into World

--- HAPI_APP/World.cpp
+++ HAPI_APP/World.cpp
@@ -1,7 +1,6 @@
 #include "World.h"
 
 
-
 World::World()
 {
 }
@@ -9,4 +8,124 @@ World::World()
 
 World::~World()
 {
+	for (auto p : entityMap)
+	{
+		delete p.second;
+	}
+}
+
+//Public
+bool World::Initialise()
+{
+	if (!HAPI_Sprites.Initialise(gameScreenSpecs.screenDimensions.x, gameScreenSpecs.screenDimensions.y, "Mediocre Wasteland Game Jam"))
+	{
+		return false;
+	}
+
+	if (!LoadSprites())
+	{
+		return false;
+	}
+
+	if (!LoadEntities()) 
+	{
+		return false;
+	}
+
+	if (!LoadWorld())
+	{
+		return false;
+	}
+
+	HAPI_Sprites.SetShowFPS(true);
+
+	return true;
+}
+
+bool World::Play()
+{
+	sprite->GetSurface()->MakeAlphaChannelFromColourKey(Colour255::BLACK);
+
+	sprite->SetAutoAnimate(100);
+
+	sprite2->SetAutoAnimate(20, true, "Right");
+
+	while (HAPI_Sprites.Update())
+	{
+		Update();
+	}
+
+	return false;
+}
+
+
+//Private
+bool World::LoadSprites()
+{
+	sprite = HAPI_Sprites.MakeSprite("Data\\helicopter.png", 4);
+
+	sprite2 = HAPI_Sprites.LoadSprite("Data\\Troll2.xml");
+
+	if (!sprite2)
+	{
+		HAPI_Sprites.UserMessage("Could not load spritesheet", "Error");
+		return false;
+	}
+
+	return true;
+}
+
+bool World::LoadEntities()
+{
+	return true;
+}
+
+bool World::LoadWorld()
+{
+	return true;
+}
+
+void World::Update()
+{
+	SCREEN_SURFACE->Clear();
+
+	GetInput();
+	CheckCollision();
+
+	sprite->GetTransformComp().SetPosition({ 500, 200 });
+	sprite->GetTransformComp().SetScaling({ 2.0f, 2.0f });
+	sprite->GetTransformComp().SetRotation(0.3f);
+
+
+	Render();
+}
+
+void World::Render()
+{
+	sprite->Render(SCREEN_SURFACE);
+	sprite2->Render(SCREEN_SURFACE);
+
+}
+
+void World::GetInput()
+{
+	//if (keyboardInput.scanCode['D'] || keyboardInput.scanCode[HK_RIGHT])
+	//{
+	//	//Move Right
+	//}
+	//
+	//if (keyboardInput.scanCode['A'] || keyboardInput.scanCode[HK_LEFT])
+	//{
+	//	//Move Left
+	//}
+
+	//if (keyboardInput.scanCode['W'] || keyboardInput.scanCode[HK_SPACE])
+	//{
+	//	//Jump
+	//}
+}
+
+bool World::CheckCollision()
+{
+	return false;
 }
--- HAPI_APP/World.h
+++ HAPI_APP/World.h
@@ -1,8 +1,45 @@
 #pragma once
+
+#include <HAPISprites_Lib.h>
+#include "HAPI_SPRITES/Shapes/Vector.h"
+#include <unordered_map>
+
+using namespace HAPISPACE;
+
 class World
 {
 public:
 	World();
 	~World();
+
+	bool Initialise();
+	bool Play();
+
+private:
+
+	struct screenSpecs
+	{
+		HAPISPACE::Vector<int> screenDimensions = HAPISPACE::Vector<int>(1280, 800);
+		HAPISPACE::BYTE* screenPointer{ nullptr };
+	};
+
+	screenSpecs gameScreenSpecs;
+	//const HAPISPACE::KeyboardData &keyboardInput = HAPI_Sprites.GetKeyboardData();
+
+	std::unordered_map <std::string, int*> entityMap;
+
+	std::shared_ptr<Sprite> sprite{ nullptr };
+	std::shared_ptr<Sprite> sprite2 { nullptr };
+
+
+	bool LoadSprites();
+	bool LoadEntities();
+	bool LoadWorld();
+
+	void Update();
+	void Render();
+
+	void GetInput();
+	bool CheckCollision();
 };
 
