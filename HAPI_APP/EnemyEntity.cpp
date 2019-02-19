111fbcded0a76ca0450334b4cfc61b506c7d2222
Jake Simpson <jsimo42@gmail.com>
2019-02-19 14:54:27 +0000

    V1.5.6
    Enemy Refactoring

--- HAPI_APP/EnemyEntity.cpp
+++ HAPI_APP/EnemyEntity.cpp
@@ -13,3 +13,100 @@ void EnemyEntity::Update()
 {
 	sprite->GetTransformComp().SetPosition(GetPosition());
 }
+
+void EnemyEntity::Update()
+{
+	AIMovement();
+	std::cout << "Px: " << m_enemyPosition.x << std::endl;
+	std::cout << "Py: " << m_enemyPosition.y << std::endl;
+}
+
+
+void EnemyEntity::AIMovement()
+{
+	if (bPatrolLevel())
+	{
+		switch (m_direction)
+		{
+		case eDirection::eUp:
+		{
+			m_enemyPosition.y -= m_movementSpeed;
+		}
+		break;
+		case eDirection::eLeft:
+		{
+			m_enemyPosition.x -= m_movementSpeed;
+		}
+		break;
+		case eDirection::eDown:
+		{
+			m_enemyPosition.y += m_movementSpeed;
+		}
+		break;
+		case eDirection::eRight:
+		{
+			m_enemyPosition.x += m_movementSpeed;
+		}
+		break;
+		default:
+			break;
+		}
+	}
+
+	if (bChasePlayer())
+	{
+		// TODO: Go In The Direction The Player Is Going
+		// TODO: Make A Map And Check Tiles Function
+	}
+
+	if (std::rand() % 100 == 0)
+	{
+		ChangeDirection((eDirection)(rand() % 4));
+	}
+
+	if (m_enemyPosition.x <= 0)
+	{
+		ChangeDirection(eDirection::eRight);
+	}
+	if (m_enemyPosition.x >= screenWidth)
+	{
+		ChangeDirection(eDirection::eLeft);
+	}
+	if (m_enemyPosition.y <= 0)
+	{
+		ChangeDirection(eDirection::eDown);
+		std::cout << "Ive Gone Off The Top Of The Screen" << std::endl;
+	}
+	if (m_enemyPosition.y >= screenHeight)
+	{
+		ChangeDirection(eDirection::eUp);
+	}
+
+
+	// TODO: Add Conditions For Enemy e.g. If They Gain/Lose Sight Of The Player Or Come Close To A Wall etc. Go In The Opposite Direction
+}
+
+void EnemyEntity::ChangeDirection(eDirection newDirection)
+{
+	m_direction = newDirection;
+}
+
+bool EnemyEntity::bPatrolLevel()
+{
+	if (!bChasePlayer())
+	{
+		return true;
+	}
+
+	return false;
+}
+
+bool EnemyEntity::bChasePlayer()
+{
+	/*if(EnemySeesPlayer)
+	{
+		return true;
+	}*/
+
+	return false;
+}
--- HAPI_APP/EnemyEntity.h
+++ HAPI_APP/EnemyEntity.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "Entity.h"
+
+
 class EnemyEntity :
 	public Entity
 {
@@ -9,5 +11,17 @@ public:
 	~EnemyEntity();
 	void Update()override final;
 	eSide GetSide() const override final { return mSide; }
+protected:
+
+private:
+	int screenWidth;
+	int screenHeight;
+	int m_movementSpeed = 1;
+	HAPISPACE::VectorI m_enemyPosition{ 500,250 };
+	eDirection m_direction;
+	void AIMovement();
+	void ChangeDirection(eDirection newDirection);
+	bool bPatrolLevel();
+	bool bChasePlayer();
 };
 
