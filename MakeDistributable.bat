cb73bff9d6438fc5048632ce24f5932343948c74
Aidan <brooksal01@gmail.com>
2019-04-30 12:46:42 +0100

    Update MakeDistributable.bat
    Edited it to Copy the folders from Data.

--- MakeDistributable.bat
+++ MakeDistributable.bat
@@ -2,7 +2,15 @@ del /s /q Demo\*.*
 rd Demo
 md Demo
 md Demo\Data
+md Demo\Data\Levels
+md Demo\Data\Sounds
+md Demo\Data\Sprites
+md Demo\Data\UserInterface
 
 copy HAPI_App\HAPI_SPRITES\Libs64 Demo\
 copy HAPI_App\Data Demo\Data\
+copy HAPI_App\Data\Levels Demo\Data\Levels
+copy HAPI_App\Data\Sounds Demo\Data\Sounds
+copy HAPI_App\Data\Sprites Demo\Data\Sprites
+copy HAPI_App\Data\UserInterface Demo\Data\UserInterface
 copy x64\Release\HAPI_APP.exe Demo\Demo.exe
