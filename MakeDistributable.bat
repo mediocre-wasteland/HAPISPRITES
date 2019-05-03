ab4d9dd697a9d67a11e022ef8ca1fabb186ae9c3
Aidan <brooksal01@gmail.com>
2019-05-03 12:51:37 +0100

    MakeDistributable
    MakeDistributable, copy folder to get running sounds.

--- MakeDistributable.bat
+++ MakeDistributable.bat
@@ -6,6 +6,7 @@ md Demo\Data\Levels
 md Demo\Data\Sounds
 md Demo\Data\Sprites
 md Demo\Data\UserInterface
+md Demo\Data\Sounds\Running
 
 copy HAPI_App\HAPI_SPRITES\Libs64 Demo\
 copy HAPI_App\Data Demo\Data\
@@ -13,4 +14,5 @@ copy HAPI_App\Data\Levels Demo\Data\Levels
 copy HAPI_App\Data\Sounds Demo\Data\Sounds
 copy HAPI_App\Data\Sprites Demo\Data\Sprites
 copy HAPI_App\Data\UserInterface Demo\Data\UserInterface
+copy HAPI_APP\Data\Sounds\Running Demo\Data\Sounds\Running
 copy x64\Release\HAPI_APP.exe Demo\Demo.exe
